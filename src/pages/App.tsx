32bd72c28e13a4f56dcbafc715c83398d22e0436
SkynixFr <romain.ludovic.day@gmail.com>
2023-04-01 11:36:05 +0200

    Build: vite, structure

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+
+import '../styles/styles.scss';
+
+const App = () => {
+	return (
+		<>
+			<Header />
+			<div>Hello world !</div>
+		</>
+	);
+};
+
+export default App;
