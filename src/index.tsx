36f778cf707aef496dd2cf9e89487d615a98f138
SkynixFr <romain.ludovic.day@gmail.com>
2023-04-01 17:31:53 +0200

    Profil: route to profil page + form basic style

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 import { createRoot } from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import App from './pages/App';
+import Profil from './pages/Profil';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container!);
 
-root.render(<App />);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />
+	},
+	{
+		path: '/profil',
+		element: <Profil />
+	}
+]);
+
+root.render(<RouterProvider router={router} />);
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,11 +7,31 @@ const App = () => {
 		<>
 			<Header />
 			<div className="search-box">
-				<input
-					type="search"
-					placeholder="Rechercher un joueur pseudo#tag"
-				/>
-				<div className="search-account">
+				<div className="search-bar">
+					<input
+						type="search"
+						placeholder="Rechercher un joueur pseudo#tag"
+					/>
+
+					<a className="search-arrow-box" href="/profil">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+							/>
+						</svg>
+					</a>
+				</div>
+
+				<div className="search-text">
 					<span>Pas de compte Riot ?</span>
 					<a
 						href="https://auth.riotgames.com/login#client_id=prod-xsso-playvalorant&code_challenge=pgSzXsy6cIXO-NXfgT281JYmO4h9ZE82KgwZ57TSqYY&code_challenge_method=S256&redirect_uri=https%3A%2F%2Fxsso.playvalorant.com%2Fredirect&response_type=code&scope=openid%20account%20email&state=9e73fbe6629e71c76ce632b5d5&uri=https%3A%2F%2Fplayvalorant.com%2Ffr-fr%2F"
