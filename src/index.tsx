8930e69800d50f3ee83d845b747524b106471250
SkynixFr <romain.ludovic.day@gmail.com>
2023-04-03 20:08:37 +0200

    Header: new content, new style

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,28 @@
 import { createRoot } from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import App from './pages/App';
 import Profil from './pages/Profil';
 import Header from './components/Header';
+import Lineups from './pages/Lineups';
+import Agents from './pages/Agents';
+import Crosshairs from './pages/Crosshairs';
+import Guides from './pages/Guides';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container!);
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '/profil',
-		element: <Profil />
-	}
-]);
-
 root.render(
-	<>
+	<Router>
 		<Header />
-		<RouterProvider router={router} />
-	</>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/profil" element={<Profil />} />
+			<Route path="/agents" element={<Agents />} />
+			<Route path="/lineups" element={<Lineups />} />
+			<Route path="/crosshairs" element={<Crosshairs />} />
+			<Route path="/guides" element={<Guides />} />
+		</Routes>
+	</Router>
 );
