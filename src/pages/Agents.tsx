19378bda8cd411f283b0228a5c57658c45359c19
SkynixFr <romain.ludovic.day@gmail.com>
2023-04-04 20:15:29 +0200

    Agents: content + style

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,5 +1,119 @@
 const Agents = () => {
-	return <div>Agents</div>;
+	return (
+		<section className="agents-container">
+			<h1 id="title">Tous les agents de Valorant</h1>
+			<div className="agent-grid">
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+				<div className="agent-card" id="card">
+					<img
+						className="agent-img"
+						src="src/assets/Sage_Artwork_Full.webp"
+						alt=""
+					/>
+					<span className="agent-name">Sage</span>
+					<span className="agent-name-vertical">Agent // Sage </span>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Agents;
